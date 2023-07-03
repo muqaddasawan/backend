a7ed5ade77f27993332645b07beb26ce052a7efa
muqaddasawan <awan.gee888@gmail.com>
2023-07-03 21:28:28 +0500

    update

--- index.js
+++ index.js
@@ -15,6 +15,33 @@ connectDB();
 
 app.use(cors());
 
+// app.use(
+//   cors({
+//     Origin: origin,
+//     preflightContinue: true,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//     optionsSuccessStatus: 200,
+//     header: "Content-Type, X-Auth-TokenExpiredError, Origin, Authorization",
+//   })
+// );
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", origin);
+//   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, X-Auth-TokenExpiredError, Origin, Authorization"
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -25,7 +52,7 @@ app.use(
 app.use("/public/products", express.static("public/products"));
 
 app.get("/", (req, res) => {
-  res.send("Backend is working...!");
+  res.send("Api working...!");
 });
 
 //Routes
